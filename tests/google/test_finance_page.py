31d2ea0cccc272e4081b96b378bd0a82d95c1195
Carlos Acosta <ceacosta.jobs@gmail.com>
2024-09-19 18:05:25 -0700

    test: verifies that expected stock symbols are in smart watchlist

--- tests/google/test_finance_page.py
+++ tests/google/test_finance_page.py
@@ -5,6 +5,11 @@ from pages.google.finance_page import GoogleFinancePage
 
 @pytest.mark.usefixtures("setup")
 class TestGoogleFinancePage:
+
+    @staticmethod
+    def get_expected_stock_symbols():
+        return ["NFLX", "MSFT", "TSLA"]
+
     def test_page_loads(self):
         finance_page = GoogleFinancePage(self.driver)
         finance_page.navigate()
@@ -22,4 +27,13 @@ class TestGoogleFinancePage:
 
         assert finance_page.smart_watchlist_stock_symbols != [], \
             "Unable to retrieve stock symbols from smart watchlist."
+
+    @pytest.mark.parametrize("expected_stock", get_expected_stock_symbols())
+    def test_stock_is_in_smart_watchlist(self, expected_stock):
+        finance_page = GoogleFinancePage(self.driver)
+        finance_page.navigate()
+
+        assert expected_stock in finance_page.smart_watchlist_stock_symbols, \
+            f"Unable to find stock symbol {expected_stock} in smart watchlist."
+
 
