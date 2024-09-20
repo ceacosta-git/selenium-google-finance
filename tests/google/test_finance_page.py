e98ba36b5cc28a227aaa74a2be1cb354572e4bfc
Carlos Acosta <ceacosta.jobs@gmail.com>
2024-09-19 17:33:32 -0700

    test: verifies that smart watchlist stock symbols can be retrieved

--- tests/google/test_finance_page.py
+++ tests/google/test_finance_page.py
@@ -1,5 +1,7 @@
 import pytest
+
 from pages.google.finance_page import GoogleFinancePage
+
 
 @pytest.mark.usefixtures("setup")
 class TestGoogleFinancePage:
@@ -7,5 +9,17 @@ class TestGoogleFinancePage:
         finance_page = GoogleFinancePage(self.driver)
         finance_page.navigate()
         expected_title = "Google Finance"
-        assert  expected_title in finance_page.title, f"Google Finance page title does not contain {expected_title}"
+        assert expected_title in finance_page.title, f"Google Finance page title does not contain {expected_title}"
+
+    def test_retrieve_smart_watchlist_stock_symbols(self):
+        finance_page = GoogleFinancePage(self.driver)
+        finance_page.navigate()
+
+        expected_smart_watchlist_section = 'You may be interested in'
+
+        assert expected_smart_watchlist_section in finance_page.smart_watchlist_section.text, \
+            f"Unable to find section: {expected_smart_watchlist_section}"
+
+        assert finance_page.smart_watchlist_stock_symbols != [], \
+            "Unable to retrieve stock symbols from smart watchlist."
 
